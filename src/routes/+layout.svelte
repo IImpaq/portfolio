ef21c72170dff48f5a1b6273fff2edc35b42d0cb
Marcus Gugacs <iimpaq@proton.me>
2023-07-19 21:30:04 +0200

    Added layouts to routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+import { fade } from "svelte/transition"
+
+import Header from "$lib/components/Header.svelte"
+import Footer from "$lib/components/Footer.svelte"
+import "$lib/styles/style.scss"
+
+export let data;
+</script>
+
+
+<Header />
+
+{#key data.currentRoute}
+  <main in:fade={{ duration: 100, delay: 100 }} out:fade={{ duration: 100 }}>
+    <slot />
+  </main>
+{/key}
+
+<Footer />
+
