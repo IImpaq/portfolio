f0c52653996c29fae385ae5f27afc93dbbc5b9db
Marcus Gugacs <iimpaq@proton.me>
2023-07-21 15:42:18 +0200

    Added missing script for dynamic resume

---
+++ src/lib/utils/home.ts
@@ -0,0 +1,23 @@
+export const fetchResume = async () => {
+  const postFiles = import.meta.glob("/src/lib/data/home/*.md");
+  const postObjs = Object.entries(postFiles);
+
+  const posts = await Promise.all(
+    postObjs.map(async ([path, resolver]) => {
+      const { metadata } = await resolver();
+
+      return {
+        title: metadata.title,
+        subtitle: metadata.subtitle,
+        description: metadata.description
+      }
+    })
+  );
+
+  const sortedPosts = posts.sort((a, b) => {
+    return (a.date < b.date);
+  });
+
+  return sortedPosts;
+}
+
