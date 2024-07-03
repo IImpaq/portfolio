e0a00dbc2d7cdaeac36861b24f828fcee67bbe7d
Marcus Gugacs <iimpaq@proton.me>
2024-07-03 19:48:39 +0200

    Added dynamic grid for hero

---
+++ app/ui/dynamic-grid.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface DynamicGridProps {
+  cellSize: number;
+  lineColor: string;
+  mousePosition: { x: number; y: number };
+}
+
+const DynamicGrid: React.FC<DynamicGridProps> = ({ cellSize, lineColor, mousePosition }) => {
+  return (
+      <div className="absolute inset-0 overflow-hidden">
+        <div
+            className="w-full h-full"
+            style={{backgroundImage: `linear-gradient(to right, ${lineColor} 1px, transparent 1px),
+                                        linear-gradient(to bottom, ${lineColor} 1px, transparent 1px)`,
+                    backgroundSize: `${cellSize}px ${cellSize}px`,
+                    mask: `radial-gradient(circle 1000px at ${mousePosition.x}px ${mousePosition.y}px,
+                            white 20%, rgba(255, 255, 255, 0.1) 100%)`,
+                    WebkitMask: `radial-gradient(circle 1000px at ${mousePosition.x}px ${mousePosition.y}px,
+                                  white 20%, rgba(255, 255, 255, 0.1) 100%)`,
+            }}
+        />
+      </div>
+  );
+};
+
+export default DynamicGrid;
