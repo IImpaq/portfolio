b626a80edf52ccb64bfcfec6aa9e7aacfc9ee9e1
Marcus Gugacs <iimpaq@proton.me>
2024-07-04 10:02:15 +0200

    Moved data to separate placeholder data file

--- app/ui/timeline-section.tsx
+++ app/ui/timeline-section.tsx
@@ -1,23 +1,14 @@
 "use client";
 
-import {FiAward, FiBook, FiBriefcase} from "react-icons/fi";
 import TimelineItem from "@/app/ui/timeline-item";
+import placeholder from "@/app/lib/placeholder-data"
 
 const TimelineSection = () => {
-  const timelineItems = [
-    {
-      icon: FiBook,
-      title: "Bachelor's in Software Engineering",
-      organization: "Graz University of Technology",
-      period: "2021 - Now"
-    },
-  ];
-
   return (
     <section className="py-20 px-4">
       <h2 className="text-3xl font-bold mb-12 text-center">Experience & Education</h2>
       <div className="max-w-3xl mx-auto">
-        {timelineItems.map((item, index) => (
+        {placeholder.timeline.map((item, index) => (
           <TimelineItem key={item.title} item={item} index={index}/>
         ))}
       </div>
