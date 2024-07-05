1452aa049f7446328be99369cac078307247bb86
Marcus Gugacs <iimpaq@proton.me>
2024-07-05 13:47:14 +0200

    Added nextjs caching functionality to supabase queries

--- app/lib/supabase/client.ts
+++ app/lib/supabase/client.ts
@@ -1,8 +1,19 @@
 import { createBrowserClient } from '@supabase/ssr'
+import {createFetch} from "@/app/lib/cache";
 
 export function createClient() {
   return createBrowserClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        global: {
+          fetch: createFetch({
+            // cache: "no-store"
+            next: {
+              revalidate: 60*60,
+            }
+          })
+        }
+      }
   )
 }
--- app/lib/supabase/server.ts
+++ app/lib/supabase/server.ts
@@ -1,5 +1,6 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
+import {createFetch} from "@/app/lib/cache";
 
 export function createClient() {
   const cookieStore = cookies()
@@ -8,6 +9,14 @@ export function createClient() {
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
+        global: {
+          fetch: createFetch({
+            // cache: "no-store"
+            next: {
+              revalidate: 60*60,
+            }
+          })
+        },
         cookies: {
           getAll() {
             return cookieStore.getAll()
